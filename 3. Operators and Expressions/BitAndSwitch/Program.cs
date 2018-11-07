802d1b2a26cfe5f5a1d48713a0ecd816a051993c
Hardik <patel.h23@gmail.com>
2018-11-06 22:06:52 -0500

    .init

---
+++ 3. Operators and Expressions/BitAndSwitch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitAndSwitch
+{
+    class Program
+    {
+
+
+        static uint ExchangeBits(uint number)
+        {
+            byte first = (byte)((number >> 3) & 7);
+            byte second = (byte)((number >> 24) & 7);
+
+            byte mix = (byte)(first ^ second);
+            number = (uint)((number) ^ (mix << 3));
+            number = (uint)((number) ^ mix << 24);
+            return number;
+        } 
+
+
+
+        static void Main(string[] args)
+        {
+            uint number = 1073709056;
+            Console.WriteLine("Old number {0,-10} to binary -> {1}", number, Convert.ToString(number, 2).PadLeft(32, '0'));
+            number = ExchangeBits(number);
+            Console.WriteLine("New number {0,-10} to binary -> {1}", number, Convert.ToString(number, 2).PadLeft(32, '0'));
+        }
+    }
+}
