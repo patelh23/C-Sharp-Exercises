86f18ba176dc4bae4d9b70a6e80b394a40cbb804
Hardik <patel.h23@gmail.com>
2018-11-08 21:30:53 -0500

    .init

---
+++ 3. Operators and Expressions/ReadsRadius/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadsRadius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter radius: ");
+            int r = int.Parse(Console.ReadLine());
+            int area = (int)(Math.PI * r * r);
+            int perimeter = (int)(2 * Math.PI * r);
+            Console.WriteLine("Area is: {0} and Perimeter is: {1}", area, perimeter);
+
+            //Write a program that reads from the console the radius "r" of a circle
+            //and prints its perimeter and area.
+        }
+    }
+}
---
+++ 3. Operators and Expressions/ReadsThreeNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadsThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reads from the console three numbers of type int
+            //and prints their sum.
+            
+            Console.WriteLine("Please enter three numbers: ");
+            int x1 = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+            int x3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sum is: " + (x1 + x2 + x3));
+        }
+    }
+}
