ae7b427c73336b4e16f381be49302ea1fd7ca8d9
Hardik <patel.h23@gmail.com>
2018-11-13 22:18:12 -0500

    .init

---
+++ 4. Console Input and Output/Range1toN/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range1toN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter integer n: ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+
+        }
+    }
+}
---
+++ 4. Console Input and Output/TotalSum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter integer n: ");
+            int n = Int32.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Please enter number:");
+                count += Int32.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("Total sum of integers is: " + count);
+
+        }
+    }
+}
