af3d2fa2bd4100585c9721a4d50b419ef55416de
Hardik <patel.h23@gmail.com>
2018-11-14 20:10:33 -0500

    .init

---
+++ 4. Console Input and Output/FibonacciSequence/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 0;
+            int sum = 1;
+            Console.Write("0,");
+            for (int i = 1; i <= 100; i++)
+            {
+                int temp = sum;
+                sum = sum + first;
+                Console.Write(sum + ",");
+                first = temp;
+            }
+
+        }
+    }
+}
