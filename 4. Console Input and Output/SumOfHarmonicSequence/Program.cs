e46f1cd22ddbd804bb804b33747c7278aa6dc978
Hardik <patel.h23@gmail.com>
2018-11-14 20:09:13 -0500

    .init

---
+++ 4. Console Input and Output/SumOfHarmonicSequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfHarmonicSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal sum = 1;
+            for (decimal i = 2; i <= 1000; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += (1 / i);
+                }
+                else
+                {
+                    sum -= (1 / i);
+                }
+
+                Console.WriteLine("{0:F3}", sum);
+            }
+
+
+        }
+    }
+}
