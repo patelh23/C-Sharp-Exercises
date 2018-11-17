26a86e9143a568b7488a05c4d3a71312c734821f
Hardik <patel.h23@gmail.com>
2018-11-17 15:27:26 -0500

    .init

---
+++ 5. Conditional Statements/IfExchanges/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfExchanges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 26;
+            int num2 = 23;
+
+            if (num1 > num2)
+            {
+                int temp = num1;
+                num1 = num2;
+                num2 = temp;
+
+            }
+            Console.WriteLine("Num1 :" + num1);
+            Console.WriteLine("Num2 :" + num2);
+        }
+    }
+}
