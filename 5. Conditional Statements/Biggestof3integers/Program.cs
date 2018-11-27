4b92d4115288eac11ace5c7e9aed22b757f06145
Hardik <patel.h23@gmail.com>
2018-11-26 19:22:38 -0500

    .init

---
+++ 5. Conditional Statements/Biggestof3integers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biggestof3integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your number: ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your number: ");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your number: ");
+            int c = Int32.Parse(Console.ReadLine());
+
+            if (a > b)
+                if (a > c) Console.WriteLine("A is the biggest");
+                else if (a < c) Console.WriteLine("C is the biggest");
+                else Console.WriteLine("A & C are the biggest");
+
+            else if (a < b)
+                if (b > c) Console.WriteLine("B is the biggest");
+                else if (b < c) Console.WriteLine("C is the biggest");
+                else Console.WriteLine("B & C are the biggest");
+
+            else if (a == b)
+                if (a == c) Console.WriteLine("All are equal");
+                else if (a < c) Console.WriteLine("C is the biggest");
+                else Console.WriteLine("A & B are equal");
+
+
+        }
+    }
+}
