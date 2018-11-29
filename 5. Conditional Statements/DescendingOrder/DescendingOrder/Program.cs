41c6947be541b7abae8c44145739988826375a4a
Hardik <patel.h23@gmail.com>
2018-11-28 20:17:39 -0500

    .init

---
+++ 5. Conditional Statements/DescendingOrder/DescendingOrder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescendingOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            int c = 11;
+
+            if (a < b)
+                if (a < c)
+                    c = c + a;
+                    a = c - a;
+                    c = c - a;
+                else if (b < c)
+                    
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
